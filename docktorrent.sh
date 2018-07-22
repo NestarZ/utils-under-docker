c46a821915187e4bdaaaf1a57f71559f830d27cc
Elias Rhouzlane <elias@macbook-pro.lan>
2018-07-22 12:46:01 +0200

    add docktorrent

---
+++ docktorrent.sh
@@ -0,0 +1,7 @@
+# docktorrent p@ssw0rd
+docker run -it \
+    -p 80:80 -p 45566:45566 -p 9527:9527/udp \
+    --dns 8.8.8.8 \
+    -v /Users/elias/Downloads/Torrents/:/rtorrent/downloads/ \
+    -e UPLOAD_RATE=1024 \
+    kfei/docktorrent
