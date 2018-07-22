c8169e1cb07b667a424d4fc15609804b240056d1
Elias Rhouzlane <elias@macbook-pro.lan>
2018-07-22 12:58:58 +0200

    correct default shared volume

--- docktorrent.sh
+++ docktorrent.sh
@@ -2,6 +2,6 @@
 docker run -it \
     -p 80:80 -p 45566:45566 -p 9527:9527/udp \
     --dns 8.8.8.8 \
-    -v /Users/elias/Downloads/Torrents/:/rtorrent/downloads/ \
+    -v /Users/elias/Downloads/Torrents/:/rtorrent/ \
     -e UPLOAD_RATE=1024 \
     kfei/docktorrent
