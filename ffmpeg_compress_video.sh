51d8c2699c4c21a24a3b21c8e6e7eea44d8cc1c8
Elias Rhouzlane <elias.rhouzlane@gmail.com>
2019-01-30 23:53:17 +0100

    fix output ffmpeg_compress_video.sh

--- ffmpeg_compress_video.sh
+++ ffmpeg_compress_video.sh
@@ -1,1 +1,1 @@
-docker run -v "${pwd}:/temp/" jrottenberg/ffmpeg -i /temp/${inputName} -vcodec libx264 -crf 20 ${outputName}
+docker run -v "${pwd}:/temp/" jrottenberg/ffmpeg -i /temp/${inputName} -vcodec libx264 -crf 20 /temp/${outputName}
